bc669bfe803571144b5d91b95e4536ed677ec215
Dhruv <tayaldhruv6@gmail.com>
2022-11-30 23:45:33 +0530

    updating

--- LInked List/SimpleLinkedList.cpp
+++ LInked List/SimpleLinkedList.cpp
@@ -61,7 +61,7 @@ bool search(node *head, int key)
     return false;
 }
 
-void deleteion(node *&head, int key)
+void deleteByNodeValue(node *&head, int key)
 {
     if (head == NULL)
     {
@@ -77,7 +77,7 @@ void deleteion(node *&head, int key)
     }
 
     node *temp = head;
-    while (temp->next->data != key)
+    while (temp->next->data != key && temp->next != NULL)
     {
         temp = temp->next;
     }
@@ -129,31 +129,111 @@ void display(node *head) // doubt
     cout << endl;
 }
 
-// int main()
-// {
-//     node *head = NULL; // doubt
-//     insertAtTail(head, 1);
-//     insertAtTail(head, 2);
-//     insertAtTail(head, 3);
-//     insertAtTail(head, 4);
-//     insertAtTail(head, 5);
-//     insertAtTail(head, 6);
-//     insertAtTail(head, 7);
+void insertAfter(node *&head, int key, int val)
+{
+    node *n = new node(val);
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            n->next = temp->next;
+            temp->next = n;
+            return;
+        }
+        temp = temp->next;
+    }
+}
 
-//     // insertAtHead(head, 6);
-//     display(head);
-//     // deleteion(head, 3);
-//     // deleteion(head, 1);
-//     // node *newhead = reverseLL(head);
-//     node *newhead = reverseRecursiveLL(head);
-//     int k = 2;
+void insertInBetween(node *&head, int index, int val)
+{
+    node *n = new node(val);
+    node *temp = head;
+    int i = 1;
+    while (i != index - 1)
+    {
+        // if (temp->data == key)
+        // {
+        // n->next = temp->next;
+        // temp->next = n;
+        // return;
+        // }
+        temp = temp->next;
+        i++;
+    }
+    n->next = temp->next;
+    temp->next = n;
+}
 
-//     // display(head);
-//     display(newhead);
+void deleteHead(node *&head)
+{
+    node *todelete = head;
+    head = head->next;
+    // free(todelete);
+    delete todelete;
+}
 
-//     // cout << search(head, 3);
-//     return 0;
-// }
+void deleteTail(node *&head)
+{
+    node *temp = head;
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    node *todelete = temp->next;
+    temp->next = NULL;
+    delete todelete;
+    // return head;
+}
+
+void deleteInBetween(node *&head, int index)
+{
+
+    int i = 1;
+    node *temp = head;
+    while (i != index - 1)
+    {
+        temp = temp->next;
+        i++;
+    }
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
+}
+
+int main()
+{
+    node *head = NULL; // doubt
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
+    insertAtTail(head, 7);
+
+    // insertAtHead(head, 6);
+    display(head);
+    // deleteHead(head);
+    // deleteTail(head);
+    // deleteInBetween(head, 5);
+    // display(newhead);
+    // insertInBetween(head, 5, 80);
+    // insertAfter(head, 80, 90);
+    deleteByNodeValue(head, 9);
+    display(head);
+    // deleteion(head, 1);
+    // node *newhead = reverseLL(head);
+    // node *newhead = reverseRecursiveLL(head);
+    // int k = 2;
+
+    // display(head);
+    // display(newhead);
+
+    // cout << search(head, 3);
+    return 0;
+}
 
 void menu()
 {
@@ -172,85 +252,85 @@ void menu()
     cout << "Enter your choice :" << endl;
 }
 
-int main()
-{
-    node *head = NULL;
-    node *newhead;
-    int ch;
-    int x;
-    int i;
-    int y;
-    do
-    {
-        menu();
-        cin >> ch;
-        switch (ch)
-        {
-        case 0:
-            cout << "Exiting :)...." << endl;
-            break;
+// int main()
+// {
+//     node *head = NULL;
+//     node *newhead;
+//     int ch;
+//     int x;
+//     int i;
+//     int y;
+//     do
+//     {
+//         menu();
+//         cin >> ch;
+//         switch (ch)
+//         {
+//         case 0:
+//             cout << "Exiting :)...." << endl;
+//             break;
 
-        case 1:
-            cout << "Enter the element :";
-            cin >> x;
-            insertAtHead(head, x);
-            display(head);
-            break;
+//         case 1:
+//             cout << "Enter the element :";
+//             cin >> x;
+//             insertAtHead(head, x);
+//             display(head);
+//             break;
 
-        case 2:
-            cout << "Enter the element :";
-            cin >> x;
-            insertAtTail(head, x);
-            display(head);
-            break;
+//         case 2:
+//             cout << "Enter the element :";
+//             cin >> x;
+//             insertAtTail(head, x);
+//             display(head);
+//             break;
 
-            // case 3:
-            //     cout << "Enter the element :";
-            //     cin >> x;
-            //     cout << "Enter the poistion to be inserted at :";
-            //     cin >> i;
-            //     l1.add_at_loc(i, x);
-            //     l1.display();
-            //     break;
+//             // case 3:
+//             //     cout << "Enter the element :";
+//             //     cin >> x;
+//             //     cout << "Enter the poistion to be inserted at :";
+//             //     cin >> i;
+//             //     l1.add_at_loc(i, x);
+//             //     l1.display();
+//             //     break;
 
-            // case 4:
-            //     l1.del_from_head();
-            //     l1.display();
-            //     break;
+//             // case 4:
+//             //     l1.del_from_head();
+//             //     l1.display();
+//             //     break;
 
-            // case 5:
-            //     l1.del_from_tail();
-            //     l1.display();
-            //     break;
+//             // case 5:
+//             //     l1.del_from_tail();
+//             //     l1.display();
+//             //     break;
 
-        case 6:
-            cout << "Enter the data to be deleted from :";
-            cin >> i;
-            deleteion(head, i);
-            display(head);
-            break;
+//         case 6:
+//             cout << "Enter the data to be deleted from :";
+//             cin >> i;
+//             deleteion(head, i);
+//             display(head);
+//             break;
 
-        case 7:
-            cout << "Enter the data to be searched : ";
-            cin >> y;
-            if (search(head, y))
-            {
-                cout << "found" << endl;
-                break;
-            }
-            cout << "not found" << endl;
-            break;
+//         case 7:
+//             cout << "Enter the data to be searched : ";
+//             cin >> y;
+//             if (search(head, y))
+//             {
+//                 cout << "found" << endl;
+//                 break;
+//             }
+//             cout << "not found" << endl;
+//             break;
 
-        case 9:
-            newhead = reverseLL(head);
-            display(newhead);
-            break;
+//         case 9:
+//             newhead = reverseLL(head);
+//             display(newhead);
+//             break;
 
-        default:
-            cout << "Invalid choice" << endl;
-            break;
-        }
+//         default:
+//             cout << "Invalid choice" << endl;
+//             break;
+//         }
 
-    } while (ch != 0);
-    return 0;
-}
+//     } while (ch != 0);
+//     return 0;
+// }
